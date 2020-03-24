6243874acf39eef3e121697497b3ee3df1839a4a
ChinDowns <webdev@chindowns.com>
2020-03-24 08:27:36 -0700

    functioning game with 4 questions selected randomly

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 // DOM Pointers
 var startBtn = document.querySelector("#start");
+var elementQuiz = document.querySelector("#quiz");
 
 // Global Variables of information the game tracks
 var correct = 0;
 var wrong = 0;
-var time = 5;
+var time = 30;
 var interval;
 var currentQuestion = [];
 var currentChoices = [];
+
 
 // start timer
 function startTimer() {
     interval = setInterval(function() {
         time--;
         document.getElementById("timer").innerHTML= "Time Remaining<br/>" + time;
-console.log(interval);
-console.log(time);
-        if ( time === 0){
+console.log("Remaing Time Is: " + time);
+        if ( time <= 0){
             stopQuiz();
         }
     },1000);
@@ -25,6 +26,15 @@ console.log(time);
 
 function stopQuiz(){
     clearInterval(interval);
+    elementQuiz.style.display = "none";
+    showResults();
+}
+
+function showResults() {
+    var elResults = document.getElementById("results-div");
+    elResults.parentElement.style.display = "block";
+    elResults.style.display = "background: grey;";
+
 }
 
 function showQuiz() {
@@ -32,49 +42,61 @@ function showQuiz() {
     document.getElementById("welcome").style.display = "none";
     
     // Show Quiz, Time and Score
-    document.getElementById("quiz").style.display = "block";
-    document.getElementById("correct").innerHTML = "Correct:<br/>" + correct;
-    document.querySelector("#wrong").innerHTML = "Wrong:<br/>" + wrong;
+    elementQuiz.style.display = "block";
    
+    showScores();
     startTimer();
     nextQuestion();
 }
 
+function showScores (){
+    document.getElementById("correct").innerHTML = "Correct:<br/>" + correct;
+    document.querySelector("#wrong").innerHTML = "Wrong:<br/>" + wrong;
+
+}
 
 function checkAnswers(event) {
     console.log(event);
     var answerBtn = event.target;
-    var index = answerBtn.getAttribute("data-answer");
-    console.log(index)
-
+    var answer = answerBtn.getAttribute("data-answer");
+    console.log("========= Selected Answer ===========")
+    console.log(answer)
+    console.log("Correct Answer is ==== " + currentQuestion.answer);
+    if (answer === currentQuestion.answer) {
+        correct++;
+    } else {
+        wrong++;
+        // take 5 seconds off the clock for a wrong answer
+        time -= 5;
+    }
+    console.log("Correct Answers are ==== " + correct);
+    console.log("Wrong Answers are ==== " + wrong);
+    showScores();
+    nextQuestion();
 }
 
 function nextQuestion() {
 
-    if(time === 0) {
-        console.log("====== time=0 at nextQuestion======");
-        return;
-    }
-     
-    // Show a random question
-    var elementQuestion = document.getElementById("question");
     currentQuestion = questions[Math.floor(Math.random()*questions.length)];
-    elementQuestion.innerHTML = currentQuestion.question;
+
+    var elQuestion = document.getElementById("question");
+    elQuestion.innerHTML = currentQuestion.question;
+
+    console.log("=============== Next Question ================");
+    console.log(currentQuestion.question);
 
     // Show answers to the question
     currentChoices = currentQuestion.choices;
     for (var i = 0; i < currentChoices.length; i++) {
-        // var attrOnClick = "checkAnswers(" + currentChoices[i] + ")";
         var elementChoice = document.getElementById("choice"+i);
         elementChoice.setAttribute("data-answer", currentChoices[i]);
-        // elementChoice.setAttribute("click", attrOnClick);
+        // Clear button then display the choice
+        elementChoice.innerHTML = "";
         elementChoice.innerHTML = currentChoices[i];
-        
     }
-
 }
 
-
+// Quiz Questions
 var questions = [{
     question: "Where is Diwali celebrated", 
     choices:["Russia", "India", "China", "Egypt"],
@@ -93,6 +115,9 @@ var questions = [{
     answer: "Spain"
 }
 ];
+
+
+
 
 // Listen for the start button to start.
 startBtn.addEventListener("click", showQuiz);
